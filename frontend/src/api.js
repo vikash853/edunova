996f0b1e3a6e68a3e70a4dcc93aeae50d660cc68
Vikash Shukla <vikashshukla5218@gmail.com>
2026-03-05 17:45:47 +0530

    correct api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://edunova-backend-wr3u.onrender.com',
+  baseURL: 'https://edunova-backend-wr3u.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
